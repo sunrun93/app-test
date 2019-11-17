afa9df1a57d2f4cce336fe0a5dad66b1bee76480
sunrun93 <wangjian9302@163.com>
2019-11-17 21:55:46 +0800

    clean proj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ZoneDemoComponent } from './zone-demo/zone-demo.component';
-import { FormDemoComponent } from './form-demo/form-demo.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MaskInputPipe } from './mask-input.pipe';
-import { InputMaskDirective } from './input-mask.directive';
-import { PhoneNumberComponent } from './phone-number/phone-number.component';
 import { FormsModule } from '@angular/forms';
-import { SSNPipe } from './form-field-error/ssn-pipe';
-import { FormFieldErrorExample } from './form-field-error/form-field-error-example';
+import { InputMaskModule } from 'primeng/inputmask';
 @NgModule({
   declarations: [
     AppComponent,
-    ZoneDemoComponent,
-    FormDemoComponent,
-    MaskInputPipe,
-    InputMaskDirective,
-    PhoneNumberComponent,
-    FormFieldErrorExample,
-    SSNPipe
+    ZoneDemoComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    InputMaskModule
   ],
-  providers: [MaskInputPipe, SSNPipe],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/form-field-error/ssn-pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'ssnMask' })
-export class SSNPipe implements PipeTransform {
-  transform(value: number): string {
-    return '123456790';
-  }
-}
